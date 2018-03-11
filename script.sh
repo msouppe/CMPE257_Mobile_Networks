716163aff435c5ad5e36ef7edeac7b7e7cee7f8c
msouppe <msouppe@gmail.com>
2018-03-11 13:30:13 -0700

    Update filenames and types

--- script.sh
+++ script.sh
@@ -7,14 +7,15 @@ import os
 import sys
 import yaml
 
+# data["random"]
 # data["randomSeed"]
 # data["numMotes"]
-# data["x_placement"]
-# data["y_placement"]
+# data["x_coord"]
+# data["y_coord"]
 # data["terrain"]
 
 # Open YAML file with all of the paramters
-f = open('simple.yaml', 'r')
+f = open('sim_config.yaml', 'r')
 
 # The search path can be used to make finding templates by
 #   relative paths much easier.  In this case, we are using
@@ -27,7 +28,7 @@ templateEnv = Environment( loader=FileSystemLoader(THIS_DIR),
                          trim_blocks=True )
 
 # This constant string specifies the template file we will use.
-TEMPLATE_FILE = "template.csc"
+TEMPLATE_FILE = "sim_template.csc"
 
 # Read the template file using the environment object.
 # This also constructs our Template object.
@@ -40,16 +41,25 @@ counter = 0
 r = javarandom.Random()
 
 for data in yaml.load_all(f):
+    
     # Random coordinate generator w/ Java random seed
     r.setSeed(data["randomSeed"])
     
+    # Choose fixed values for the x,y coordinates for motes
+    if (data["random"] == False ):
+        templateVars = { "RANDOM_SEED" : str(data["randomSeed"]),
+                           "MOTES" : data["numMotes"],
+                         "X_COORD" : data["x_coord"],
+                         "Y_COORD" : data["y_coord"]    }
+    
+    # Else use random generator to choose x,y mote coordinates
 	# Specify any input variables to the template as a dictionary.
     templateVars = { "RANDOM_SEED" : str(data["randomSeed"]),
                            "MOTES" : data["numMotes"],
                          "X_COORD" : r.nextInt(8000),
                          "Y_COORD" : r.nextInt(6000)    }
 
-    # Finally, process the template to produce our final text.
+    # Process the template to produce our final text.
     outputText = template.render( templateVars )
 
     # Create new file simulation with write privalages
@@ -58,5 +68,6 @@ for data in yaml.load_all(f):
     # New file with new variables
     f.write(outputText)
 
+    # Counter used for simulation filename index
     counter += 1
 
