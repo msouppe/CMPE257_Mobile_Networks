9564b33a674c599cbe1a1be825517e3215d77a12
Mariette Souppe <msouppe@gmail.com>
2018-03-19 22:27:12 -0700

    Upload pre-docker

---
+++ terrain_exp/pipelines/2_5DTerrain/post-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# [wf] series of steps required to execute the pipeline.
+# This file should contain the series of steps that are required to execute
+# the pipeline. Any non-zero exit code will be interpreted as a failure
+# by the 'popper check' command.
+set -e
+
+# add commands here:
+
+exit 0
---
+++ terrain_exp/pipelines/2_5DTerrain/teardown.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# [wf] cleanup tasks.
+# Put all your cleanup tasks here.
+set -e
+exit 0
