1abfdb667fadc63918ce4da69a31c7c2afcdfcda
Ivo Jimenez <ivo.jimenez@gmail.com>
2018-03-21 19:24:31 -0700

    cleanup paper folder

--- paper/pipelines/single-node/results/visualize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-  --name jupyter_`basename $PWD` \
-  -p 8888:8888 \
-  -v `pwd`:/home/jovyan/work ivotron/scipy-notebook \
-  start-notebook.sh --NotebookApp.token=""
