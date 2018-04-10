fea865cdcce35f652b1881e55da75f315b79be77
msouppe <msouppe@users.noreply.github.com>
2018-04-09 23:19:55 -0700

    Update run.sh

--- pipelines/terrain/run.sh
+++ pipelines/terrain/run.sh
@@ -5,30 +5,12 @@
 # by the 'popper check' command.
 set -ex
 
-# 
-# cd simulations/
-
-# Get 
-# absolutePath=$(pwd)
-
-# Go into Cooja folder to run simulations.
-# cd ../contiki/tools/cooja
-
-#outputPath=../../../output
-#logPath=contiki/tools/cooja/build/COOJA.testlog
-
-#JAVA_HOME=/usr ; export JAVA_HOME
-
-# Get each file in the simulation directory and save to variable.
-# filenames=$(ls simulations/)
-filenames=simulations/*.csc
-
 # Run simulation files in simulation folder in contiki.
 for file in simulations/*.csc; do
    echo "$file"
    # ant run_nogui -Dargs="$absolutePath/$file" 
    docker run -v `pwd`:/experiment --workdir=/experiment/contiki/tools/cooja --entrypoint=ant terrain run_nogui -Dargs=/experiment"/$file"
-   # docker cp contiki/tools/cooja/build/COOJA.testlog output/"${file}_log".log
+   docker cp contiki/tools/cooja/build/COOJA.testlog output/"${file}_log".log
    # cp $logPath $outputPath/"${file}_log".log
 done
 
